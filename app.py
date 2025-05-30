f80030b19307114735b27da08570139f255dea16
Aditonomy <152847182+aditnojam@users.noreply.github.com>
2025-05-30 09:21:25 +0700

    newup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import subprocess
-from stress_analysis import analisis_stres
+import json  # untuk baca hasil dari file
 
 app = Flask(__name__)
 
@@ -11,16 +11,27 @@ def index():
 @app.route('/result', methods=['POST'])
 def result():
     username = request.form['username']
-    
-    # Simpan username ke file agar bisa dibaca oleh crawling.ipynb
+
+    # Simpan username ke file
     with open('username.txt', 'w') as f:
         f.write(username)
-    
-    # Jalankan crawling.ipynb
-    subprocess.run(["jupyter", "nbconvert", "--to", "notebook", "--execute", "crawling.ipynb", "--output", "executed_crawling.ipynb"])
 
-    # Lanjutkan ke analisis
-    hasil = analisis_stres()
+    # Jalankan crawling.ipynb
+    subprocess.run([
+        "jupyter", "nbconvert", "--to", "notebook", "--execute",
+        "crawling.ipynb", "--output", "executed_crawling.ipynb"
+    ])
+
+    # Jalankan analysis.ipynb
+    subprocess.run([
+        "jupyter", "nbconvert", "--to", "notebook", "--execute",
+        "analysis.ipynb", "--output", "executed_analysis.ipynb"
+    ])
+
+    # Baca hasil dari file JSON (misalnya hasil disimpan sebagai hasil.json oleh analysis.ipynb)
+    with open('hasil.json', 'r', encoding='utf-8') as f:
+        hasil = json.load(f)  # list of [tweet, label, prob]
+
     return render_template('result.html', hasil=hasil)
 
 if __name__ == '__main__':
